69bdcf99ab20963fb8a90d9f693d322dc1119a9a
youssef youssef <youssef.youssef@kardex.com>
2023-12-09 14:27:34 +0100

    Added a new function that outputs the best and the worst result

--- test.py
+++ test.py
@@ -1,7 +1,41 @@
 Zahl1 = int(input("Geben Sie ihre erste Zahl ein:"))
+
 Zahl2 = int(input("Geben Sie ihre zweite Zahl ein:"))
 print("hi")
 print(Zahl1 * Zahl2)
 
 print("Hello Youssef")
 print("Thats a discord test")
+
+
+print("*****************")
+#wurfprogramm 
+wuerfe = []
+
+for i in range(6):
+    wuerfe.append(int(input("wurf:")))
+
+maxP=wuerfe[0]
+minP=wuerfe[0]
+summeP=0
+
+for i in range(6):
+    if maxP< wuerfe[i]:
+        maxP= wuerfe[i]
+
+for i in range(6):
+    if minP> wuerfe[i]:
+        minP= wuerfe[i]
+
+for i in range(6):
+    summeP=summeP+wuerfe[i]
+
+mittelP = summeP/len(wuerfe)
+
+print("Bester wurf:",maxP)
+
+print("Schlechtester wurf:",minP)
+
+print("Durschschnittliche Punktzahl:",mittelP)
+
+

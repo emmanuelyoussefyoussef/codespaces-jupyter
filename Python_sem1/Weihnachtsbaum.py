9a8e8b7a13100043869d811533881752d4fc941d
youssef youssef <youssef.youssef@kardex.com>
2023-12-17 22:13:46 +0100

    mein code kann jetzt weihnachtsbäume zeichnen

---
+++ Python_sem1/Weihnachtsbaum.py
@@ -0,0 +1,30 @@
+import time
+
+print("Willkommen zum Weihnachtsbaum Projekt!!")
+print("""
+         *
+        ***
+       *****
+      *******
+     *********
+    ***********
+         *
+         *
+      """)
+#speicher für die Sterne zum ausdrucken
+zwischenspeicher=1
+x=0
+#Eingabe der Größe des Baumes
+groesse=int(input("Wie groß soll der Weihnachtsbaum sein?"))
+for x in range(groesse):
+    #Der erste befehl in print sorgt für den Abstand und der zweite für die Sterne
+    print(" " * (groesse - 1*x), zwischenspeicher * "*")
+    #Sterne anz7ahl erhöht sich jeden durchlauf
+    zwischenspeicher = zwischenspeicher + 2
+    #Delay
+    time.sleep(0.2)
+    #Neue schleife die den Ast printet
+    if x == groesse-1:
+        print(" " *(groesse), "*")
+        print(" " *(groesse), "*")
+        time.sleep(1)

8c7fecf4c94610fc4e87af95efd0117216f610b3
youssef youssef <youssef.youssef@kardex.com>
2023-12-18 14:36:41 +0100

    turtle

---
+++ Python_sem1/turte.py
@@ -0,0 +1,13 @@
+import turtle  #Inside_Out
+wn = turtle.Screen()
+wn.bgcolor("light green")
+skk = turtle.Turtle()
+skk.color("blue")
+ 
+def sqrfunc(size):
+    for i in range(4):
+        skk.fd(size)
+        skk.left(90)
+        size = size +5
+for i in range(100):
+    sqrfunc(6+(i*20))

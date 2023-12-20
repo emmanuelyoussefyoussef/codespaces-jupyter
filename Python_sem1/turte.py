a619c2496ef2d1c813349f43cec2c8d92cdb3071
youssef youssef <youssef.youssef@kardex.com>
2023-12-20 15:08:44 +0100

    Fix

--- Python_sem1/turte.py
+++ Python_sem1/turte.py
@@ -2,27 +2,27 @@
 from turtle import Turtle, Screen
 turtle_size=20
 skk = Turtle(shape="turtle", visible=True)
-wn = Screen()
-wn.bgcolor("beige")
+Scr = Screen()
+Scr.bgcolor("beige")
 skk.speed(10)
 
 
 def moving():
     skk.forward(4)
-    wn.ontimer(moving,10)
+    Scr.ontimer(moving,10)
 
-wn.onkey(lambda: skk.setheading(90), 'Up')
-wn.onkey(lambda: skk.setheading(270), 'Down')
-wn.onkey(lambda: skk.setheading(0),'Right')
-wn.onkey(lambda: skk.setheading(180),'Left')
-
-
+Scr.onkey(lambda: skk.setheading(90), 'Up')
+Scr.onkey(lambda: skk.setheading(270), 'Down')
+Scr.onkey(lambda: skk.setheading(0),'Right')
+Scr.onkey(lambda: skk.setheading(180),'Left')
 
 
 
 
-wn.listen()
+
+
+Scr.listen()
 
 moving()
 
-wn.mainloop()
+Scr.mainloop()

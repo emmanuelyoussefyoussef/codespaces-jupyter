aaf4b9622f1ff8543f3dfb4a182bf88ab36d769e
youssef youssef <youssef.youssef@kardex.com>
2023-12-20 14:27:48 +0100

    Feat: turtle can now write INF

--- Python_sem1/turte.py
+++ Python_sem1/turte.py
@@ -1,13 +1,69 @@
-import turtle  #Inside_Out
+# Python program to draw square 
+# using Turtle Programming
+import turtle 
+turtle_size=20
+skk = turtle.Turtle(shape="turtle", visible=False)
 wn = turtle.Screen()
-wn.bgcolor("light green")
-skk = turtle.Turtle()
-skk.color("blue")
- 
-def sqrfunc(size):
-    for i in range(4):
-        skk.fd(size)
-        skk.left(90)
-        size = size +5
-for i in range(100):
-    sqrfunc(6+(i*20))
+wn.bgcolor("beige")
+skk.speed(10)
+
+skk.penup()
+skk.goto(-350,300)
+skk.pendown()
+skk.showturtle()
+
+skk.forward(100)
+skk.backward(50)
+skk.right(90)
+skk.forward(300)
+skk.right(90)
+skk.forward(50)
+skk.right(180)
+skk.forward(100)
+
+skk.penup()
+skk.forward(50)
+skk.pendown()
+
+#N
+
+skk.left(90)
+skk.forward(300)
+skk.right(180)
+
+skk.left(25)
+skk.forward(320)
+skk.left(155)
+
+
+skk.forward(300)
+skk.right(90)
+
+skk.penup()
+skk.forward(50)
+skk.pendown()
+
+#F
+
+skk.forward(180)
+skk.backward(180)
+skk.right(90)
+skk.forward(300)
+skk.backward(140)
+skk.left(90)
+skk.forward(140)
+
+
+
+
+# I ist 300 hoch
+
+
+
+
+#for i in range(4):
+#	skk.forward(50)
+#	skk.right(90)
+
+	
+turtle.done()

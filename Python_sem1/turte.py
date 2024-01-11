04cdff8f1b908e718c8e3500bdacceaf9fb41b19
youssef youssef <youssef.youssef@kardex.com>
2024-01-11 19:25:59 +0100

    NOT FINISHED: working on a keybind that stops the turtle

--- Python_sem1/turte.py
+++ Python_sem1/turte.py
@@ -7,6 +7,7 @@
 
 from turtle import Turtle, Screen
 import random
+import keyboard
 
 turtle_size=20
 skk = Turtle(shape="turtle", visible=True)
@@ -17,15 +18,26 @@ skk.penup()
 
 color1= ["red","blue","black","green","cyan","magenta"]
 def moving():
-    skk.forward(7)
-    Scr.ontimer(moving,50)
+        skk.forward(7)
+        Scr.ontimer(moving,50)
+
+def check_key_pressed(key):
+    if keyboard.is_pressed(key)=='b':
+         skk.forward(7)
+         Scr.ontimer(moving,50)
+    elif keyboard.is_pressed(key)=='w':
+         skk.forward(0)
+         Scr.ontimer(moving,0)
+         check_key_pressed('w')
+
 
 def color():
     random2= "black"
     random2= random.choice(color1)
     skk.pencolor(random2)
 
-
+Scr.onkey(lambda: check_key_pressed('b'),'b')
+Scr.onkey(lambda: check_key_pressed('w'),'w')
 Scr.onkey(lambda: skk.setheading(90), 'Up')
 Scr.onkey(lambda: skk.setheading(270), 'Down')
 Scr.onkey(lambda: skk.setheading(0),'Right')
@@ -37,7 +49,7 @@ Scr.onkey(lambda: skk.clear(), 'space')
 
 
 Scr.listen()
-
 moving()
 color()
+
 Scr.mainloop()

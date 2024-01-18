718b201add7e523c33c4b4d03006c65edb4bd77c
youssef youssef <youssef.youssef@kardex.com>
2024-01-18 17:37:32 +0100

    Feat: a 2nd player is now added

--- Python_sem1/turte.py
+++ Python_sem1/turte.py
@@ -9,43 +9,33 @@ from turtle import Turtle, Screen
 import random
 import keyboard
 
+#player 1
 turtle_size=20
 skk = Turtle(shape="turtle", visible=True)
 Scr = Screen()
 Scr.bgcolor("beige")
 skk.speed(10)
 skk.penup()
-running=True
-#player 2
-#pla2= Turtle(shape="turtle", visible=True)
-#pla2.speed(10)
-#pla2.penup()
-#def moving_pla2():
-#        pla2.forward(7)
-
-color1= ["red","blue","black","green","cyan","magenta"]
-def moving():
-        if running==True:
+runningp1=False
+skk.setx(-250)
+skk.sety(250)
+#moving player 1
+def movingp1():
+        if runningp1==True:
             skk.forward(7)
-            Scr.ontimer(moving,50)
-        
-
-def check_key_pressed(key):
-    global running
+            Scr.ontimer(movingp1,50)
+#keybind check player1
+def check_key_pressedp1(key):
+    global runningp1
     if key=='l':
-         running=False
+         runningp1=False
     elif key=='o':
-         running=True
-         moving()
+         runningp1=True
+         movingp1()
 
-
-def color():
-    random2= "black"
-    random2= random.choice(color1)
-    skk.pencolor(random2)
-
-Scr.onkey(lambda: check_key_pressed('l'),'l')
-Scr.onkey(lambda: check_key_pressed('o'),'o')
+#keybinds player 1
+Scr.onkey(lambda: check_key_pressedp1('l'),'l')
+Scr.onkey(lambda: check_key_pressedp1('o'),'o')
 Scr.onkey(lambda: skk.setheading(90), 'Up')
 Scr.onkey(lambda: skk.setheading(270), 'Down')
 Scr.onkey(lambda: skk.setheading(0),'Right')
@@ -54,15 +44,49 @@ Scr.onkey(lambda: skk.penup(),'c')
 Scr.onkey(lambda: (skk.pendown(),color()), 'd')
 Scr.onkey(lambda: skk.clear(), 'space')
 
-#Scr.onkey(lambda: pla2.setheading(90), '8')
-#Scr.onkey(lambda: pla2.setheading(270), '2')
-#Scr.onkey(lambda: pla2.setheading(0),'6')
-#Scr.onkey(lambda: pla2.setheading(180),'4')
+
+#player 2
+pla2= Turtle(shape="turtle", visible=True)
+pla2.speed(10)
+pla2.penup()
+runningp2=False
+pla2.setx(250)
+pla2.sety(250)
+
+def movingp2():
+        if runningp2==True:
+            pla2.forward(7)
+            Scr.ontimer(movingp2,50)
+
+def check_key_pressedp2(key):
+    global runningp2
+    if key=='0':
+         runningp2=False
+    elif key==',':
+         runningp2=True
+         movingp2()
+
+Scr.onkey(lambda: pla2.setheading(90), '8')
+Scr.onkey(lambda: pla2.setheading(270), '2')
+Scr.onkey(lambda: pla2.setheading(0),'6')
+Scr.onkey(lambda: pla2.setheading(180),'4')
+Scr.onkey(lambda: check_key_pressedp2('0'),'0')
+Scr.onkey(lambda: check_key_pressedp2(','),',')
+
+
+
+color1= ["red","blue","black","green","cyan","magenta"]
+
+def color():
+    random2= "black"
+    random2= random.choice(color1)
+    skk.pencolor(random2)
+
 
 
 Scr.listen()
-moving()
-#moving_pla2()
+movingp1()
+movingp2()
 color()
 
 Scr.mainloop()
